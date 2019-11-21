b1c88f644ead0e435e517c2ca8fe3ee9142095d9
yushangyubeicheng <1220703694@qq.com>
2019-11-21 12:01:17 +0800

    注册中心搭建

--- api/src/main/java/com/suntek/itoken/provider/api/api/ApiApplication.java
+++ api/src/main/java/com/suntek/itoken/provider/api/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.suntek.itoken.api.api;
+package com.suntek.itoken.provider.api.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
